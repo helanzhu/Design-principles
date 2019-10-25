89c4e4d619663134249c60dadffca5fe78bbf3f0
chenqg <chenqg3721@gmail.com>
2019-10-25 17:46:39 +0800

    初始化

---
+++ Design-principles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Design-principles
+//
+//  Created by chenqg on 2019/10/25.
+//  Copyright © 2019年 helanzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Design-principles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Design-principles
+//
+//  Created by chenqg on 2019/10/25.
+//  Copyright © 2019年 helanzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
